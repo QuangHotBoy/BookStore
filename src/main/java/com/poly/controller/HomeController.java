819ea633bd2be7ac28b41175b636f18ff8038209
Quang đẹp trai nhất Trái Đất <133572908+QuangHotBoy@users.noreply.github.com>
2024-01-15 20:42:48 +0700

    a

--- src/main/java/com/poly/controller/HomeController.java
+++ src/main/java/com/poly/controller/HomeController.java
@@ -7,18 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.DAO.InventoryDAO;
 import com.poly.DAO.ProductDAO;
 import com.poly.DAO.UsersDAO;
 import com.poly.entity.Inventory;
 import com.poly.entity.Products;
-import com.poly.entity.Users;
 import com.poly.service.CookieService;
 import com.poly.service.ParamService;
 import com.poly.service.SessionService;
-
-import jakarta.servlet.http.Cookie;
 
 @Controller
 public class HomeController {
@@ -30,57 +28,60 @@ public class HomeController {
 
 	@Autowired
 	UsersDAO udao;
-	
+
 	@Autowired
 	InventoryDAO invenDao;
-	
+
 	@Autowired
 	CookieService cook;
-	
+
 	@Autowired
 	ParamService param;
-	
-	
+
+	@RequestMapping("index")
+	public String index() {
+		return "/home/index";
+	}
+
 	// có vấn đề để sau
 	@GetMapping("/home/watch")
 	public String getHome(Model m) {
 		Products p = dao.findByKeywordsBySQL();
-//		String ucheck = cook.getValue("checkEmail");
+		// String ucheck = cook.getValue("checkEmail");
 		String ucheck = ssSer.getAttribute("usercheck");
-		if(ucheck == null) {
+		if (ucheck == null) {
 			m.addAttribute("hidden", false);
 			List<Products> items = dao.findByKeywordsAllBySQL();
 			m.addAttribute("item", p);
 			m.addAttribute("items", items);
-		}else {
-			m.addAttribute("hidden",true);
+		} else {
+			m.addAttribute("hidden", true);
 			List<Products> items = dao.findByKeywordsAllBySQL();
 			m.addAttribute("item", p);
 			m.addAttribute("items", items);
 		}
-		
-		
+
 		return "/home/index";
 	}
-	
+
 	@GetMapping("/home/search")
 	public String SearchProduct(Model m) {
 		Products p = dao.findByKeywordsBySQL();
 		String ucheck = cook.getValue("checkEmail");
 		String search = param.getString("search", "");
-		
-		if(ucheck==null) {
+
+		if (ucheck == null) {
 			m.addAttribute("hidden", false);
 			List<Products> items = dao.findByKeywordsAllBySQL(search);
 			m.addAttribute("item", null);
 			m.addAttribute("items", items);
-		}else {
-			m.addAttribute("hidden",true);
+		} else {
+			m.addAttribute("hidden", true);
 			List<Products> items = dao.findByKeywordsAllBySQL(search);
 			m.addAttribute("item", null);
 			m.addAttribute("items", items);
 		}
-		
+
 		return "/home/index";
 	}
 
@@ -89,21 +90,20 @@ public class HomeController {
 
 		Products p = dao.findById(id).get();
 		Inventory inven = invenDao.findObject(id);
-		
+
 		m.addAttribute("inven", inven);
 		m.addAttribute("p", p);
 
 		return "/home/detailWatched";
 	}
 
-	
 	// xét điều kiện chưa đăng nhập chọn vào trang cá nhân
 	//
 	@GetMapping("/home/profile")
 	public String getProfile(Model m) {
 		String u = ssSer.getAttribute("usercheck");
-//		Users u = ssSer.getAttribute("username");
-		if(u == null) {
+		// Users u = ssSer.getAttribute("username");
+		if (u == null) {
 			m.addAttribute("userNull", true);
 			m.addAttribute("hidden", false);
 			Products p = dao.findByKeywordsBySQL();
@@ -111,7 +111,7 @@ public class HomeController {
 			m.addAttribute("item", p);
 			m.addAttribute("items", items);
 			return "/home/index";
-		}else {
+		} else {
 			return "redirect:/account/profileUser";
 		}
 
